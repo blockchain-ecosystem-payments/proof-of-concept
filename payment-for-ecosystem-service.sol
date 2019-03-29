536269ce6a6f6e97d028734caa61548a25610bfc
blockchain-ecosystem-payments <49047370+blockchain-ecosystem-payments@users.noreply.github.com>
2019-03-29 16:50:35 +0000

    Update payment-for-ecosystem-service.sol

--- payment-for-ecosystem-service.sol
+++ payment-for-ecosystem-service.sol
@@ -10,17 +10,17 @@ contract PaymentForEcosystemServices is usingOraclize {
     address payable constant PES_buyer = 0x2C1179A512156F0d937ce30ef315046e3B9E50Aa;
     address payable constant PES_seller = 0xEEF8Acd8C3530f28b61549fA5012E0A462E8A67F;
     
-    // Initialize PES payment by querying the Oraclize computation datasource
-    function InitializePES () public payable {
-        if(msg.sender != PES_buyer) revert();
-        oraclize_query(1553008500, "computation", ["BAisPZsJU/Tkn4zfquD+ByizlGfntI5Ej3qgREdbG3Lt54c6MjG2HgYcNcx6yifbr1Sa3UE33dVlrz3w8xCwIkkgLcd13GQPuLC8m/mWmR0Pv/lMCh8BXNWAMsG3dFLk8m8luccncIwfVjtqNwDNcKqoAk8RqNZT3+2LO5cqxQ=="]);
-    }
-    
     // Oraclize callback function
     function __callback(bytes32 _myid, string memory _result) public {
         if(msg.sender != oraclize_cbAddress()) revert();
         percentage_landcover_change = parseInt(_result);
         TransferPES();
+    }
+    
+    // Initialize PES payment by querying the Oraclize computation datasource
+    function InitializePES () public payable {
+        if(msg.sender != PES_buyer) revert();
+        oraclize_query("computation", ["BAisPZsJU/Tkn4zfquD+ByizlGfntI5Ej3qgREdbG3Lt54c6MjG2HgYcNcx6yifbr1Sa3UE33dVlrz3w8xCwIkkgLcd13GQPuLC8m/mWmR0Pv/lMCh8BXNWAMsG3dFLk8m8luccncIwfVjtqNwDNcKqoAk8RqNZT3+2LO5cqxQ=="]);
     }
     
     // Executing the payment and transfer remaining balance back to PES buyer's account
